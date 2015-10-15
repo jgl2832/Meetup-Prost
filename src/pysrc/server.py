44bc738c7d568f7411a2b10a7d7ca4c6a8812bb9
Jake <jake@meetup.com>
2015-10-15 17:44:19 -0400

    some pages

--- src/pysrc/server.py
+++ src/pysrc/server.py
@@ -1,11 +1,18 @@
 import cherrypy
+
+from mako.template import Template
+from mako.lookup import TemplateLookup
+
 from api import open_events_global
+
+lookup = TemplateLookup(directories=['html'])
 
 class Root(object):
 	@cherrypy.expose
-	def index(self):
-		res = open_events_global("prost").results
-		return str([r.name for r in res])
+	def index(self, query="prost"):
+		tmpl = lookup.get_template("index.html")
+		res = open_events_global(query).results
+		return tmpl.render(results=res, query=query)
 
 if __name__ == '__main__':
 	cherrypy.quickstart(Root(), '/')
