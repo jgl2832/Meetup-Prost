38be2e2d5d63c199423c3cf8f03cb7df8766c30e
Jake <jake@meetup.com>
2015-10-15 12:38:02 -0400

    codecs and such

--- src/pysrc/api.py
+++ src/pysrc/api.py
@@ -4,7 +4,9 @@ except ImportError:
 	print "Must define key.py according to readme"
 	exit(1)
 
-import urllib, urllib2
+import json
+import urllib
+import urllib2
 
 ROOT = "https://api.meetup.com"
 OPEN_EVENTS = "/2/open_events"
@@ -21,7 +23,7 @@ def _params_string(**kwargs):
 def _make_path(root, path, query):
 	return "%s%s?%s" % (root, path, query)
 
-def _fetch_from_api(root=ROOT, path="/", **kwargs):
+def _fetch_raw_from_api(root=ROOT, path="/", **kwargs):
 	full_path = _make_path(root, path, _params_string(**kwargs))
 	try:
 		response = urllib2.urlopen(full_path)
@@ -32,6 +34,35 @@ def _fetch_from_api(root=ROOT, path="/", **kwargs):
 		return None
 
 
+def _fetch_from_api(root=ROOT, path="/", **kwargs):
+	raw = _fetch_raw_from_api(root, path, **kwargs)
+	return raw and json.loads(raw)
+
 ### Endpoints ###
-def open_events(query):
-	return _fetch_from_api(path=OPEN_EVENTS, text=query)
+def open_events(query, lat, lon, radius):
+	return ApiResult(_fetch_from_api(path=OPEN_EVENTS, text=query, lat=lat, lon=lon, radius=radius), EventResult)
+
+### Codecs ###
+
+class ApiResult():
+	def __init__(self, json, result_serializer):
+		self.results = [result_serializer(r) for r in json["results"]]
+		self.meta = json["meta"]
+	def __str__(self):
+		return "Results: %s, Meta: %s" % (str(self.results), str(self.meta))
+
+class EventResult():
+	attrs_to_grab = ["name"]
+	def __init__(self, json):
+		group_json = json["group"]
+
+		self.name = json["name"]
+		self.distance = json["distance"] if "distance" in json else None
+
+		if "venue" in json:
+			venue_json = json["venue"]
+			self.lat = venue_json["lat"]
+			self.lon = venue_json["lon"]
+		else:
+			self.lat = group_json["group_lat"]
+			self.lon = group_json["group_lon"]
