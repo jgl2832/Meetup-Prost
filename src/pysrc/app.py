f3dd02c00daa63d45a3397a4984889fb6accce6d
Jake <jake@meetup.com>
2015-10-15 12:00:01 -0400

    framework for making api request

--- src/pysrc/app.py
+++ src/pysrc/app.py
@@ -1,5 +1,41 @@
+try:
+	from key import key
+except ImportError:
+	print "Must define key.py according to readme"
+	exit(1)
+
+import urllib, urllib2
+
+ROOT = "https://api.meetup.com"
+OPEN_EVENTS = "/2/open_events"
+
+def _params_dict(**kwargs):
+	params = kwargs
+	params["key"] = key
+	return params
+
+def _params_string(**kwargs):
+	param_dict = _params_dict(**kwargs)
+	return urllib.urlencode(param_dict)
+
+def _make_path(root, path, query):
+	return "%s%s?%s" % (root, path, query)
+
+def fetch_from_api(root=ROOT, path="/", **kwargs):
+	full_path = _make_path(root, path, _params_string(**kwargs))
+	try:
+		response = urllib2.urlopen(full_path)
+		html = response.read()
+		return html
+	except urllib2.HTTPError, e:
+		print "Unable to fetch from %s: %s" % (full_path, e)
+		return None
+
+def open_events(query):
+	return fetch_from_api(path=OPEN_EVENTS, text=query)
+
 def main():
-	print "Hello World!"
+	print open_events("prost")
 
 if __name__ == "__main__":
 	main()
