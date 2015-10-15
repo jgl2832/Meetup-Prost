5efd71f7dbc80755556200b037ef3dad6871bccf
Jake <jake@meetup.com>
2015-10-15 13:13:37 -0400

    server run

--- src/pysrc/server.py
+++ src/pysrc/server.py
@@ -1,9 +1,11 @@
 import cherrypy
+from api import open_events_global
 
 class Root(object):
 	@cherrypy.expose
 	def index(self):
-		return "Hello World!"
+		res = open_events_global("prost").results
+		return str([r.name for r in res])
 
 if __name__ == '__main__':
 	cherrypy.quickstart(Root(), '/')
